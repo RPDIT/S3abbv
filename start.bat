21264adb0a1042884dc17b0cf794b93c8d2dbd11
RPDIT <RPDoherty.IT@gmail.com>
2023-11-19 17:18:45 -0500

    Removed additional variables

--- start.bat
+++ start.bat
@@ -1,8 +1,6 @@
 echo off
-set arg1=%1
-set arg2=%~dp0
-set arg3=\s3abbv.py
-set filepath=%0\..\%arg3%
+set arg=\s3abbv.py
+set filepath=%0\..\%arg%
 FOR /f %%p  in ('where python') do SET PYTHONPATH=%%p
 shift
 %PYTHONPATH% %filepath%
