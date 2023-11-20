0e187a48b4db4cfaacf1712ff86913c7c23bb1ec
RPDIT <RPDoherty.IT@gmail.com>
2023-11-20 10:19:59 -0500

    Fixed typo.

--- s3abbv.py
+++ s3abbv.py
@@ -15,5 +15,5 @@ data = stateData.getDataFromFile()
 
 while True:
     main()
-    if input("Do you with to continue? Enter X to close.") == "X":
+    if input("Do you wish to continue? Enter X to close.") == "X":
         break
